6a76f50f05e2b003c05c62316d56df9694eef482
Mora Kopech <morakopech@gmail.com>
2023-10-23 17:18:30 -0300

    2.0

--- Entregable 1/Ejercicio_2.js
+++ Entregable 1/Ejercicio_2.js
@@ -1,1 +1,16 @@
 //Implemente su propia funcion find
+
+//buscar articulo con mayor csntidad de stock
+
+let m =new Map([[2, "Camperas"], [3, "Pantalones"], [8, "Remeras"]]);
+
+for (let [key, value] of m) 
+{
+    return a = Math.max(key);
+}
+console.log(a);
+
+const f = m.find((key, value) => Math.max(key));
+
+console.log(f);
+
