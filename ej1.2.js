2366a5641dbe215162cc4b34614e438efb1ba700
Mora Kopech <morakopech@gmail.com>
2023-10-31 06:57:45 -0300

    .

--- ej1.2.js
+++
@@ -1,9 +0,0 @@
-// Implemente su propia funcion find
-
-let m = [1, 2, 5, 100, 8, 74];
-
-let element = Math.min(...m);
-console.log(element);
-
-const f = m.find(e => e === element);
-console.log(f);
